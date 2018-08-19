a2a49b959d189db17f3f21531c3a424ccdc15b0d
emma-nio <emmanioashley@gmail.com>
2018-08-19 14:01:47 +1200

    created index.js file and functions which are callback functions for getFiles and openFiles. welcomeMessage (not a callback function) displayList and displayFile

---
+++ index.js
@@ -0,0 +1,66 @@
+const fs = require('fs')
+const path = require('path')
+
+const getFiles = require('./getFiles')
+const openFiles = require('./openFiles')
+const readline = require('readline')
+const data = __dirname + '/data'
+
+function welcomeMessage () {
+  const message = 'Hi There!'
+  console.log(message)
+  return message
+}
+
+function start () {
+  getFiles(data, displayList)
+}
+
+start()
+
+// getFiles callback function
+function displayList (err, files) {
+  let list = ''
+  for (let i = 0; i < files.length; i++) {
+    list += i + ' ' + files[i] + '\n'
+  }
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+  rl.question('Please select a number from the list then press Enter... \n' + list, (input) => {
+    rl.close()
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.error(err)
+    } else {
+      let i = Number(input)
+      if (i < files.length) {
+        openFiles(files[i], displayFile)
+      } else {
+        // eslint-disable-next-line no-console
+        console.log('\n woops that number isnt on our list! \n')
+        getFiles(data, displayList)
+      }
+    }
+  })
+}
+
+// openFiles callback function
+function displayFile (err, imageFile) {
+  if (err) {
+    // eslint-disable-next-line no-console
+    console.error(err)
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(imageFile)
+    getFiles(data, displayList) // displays list again for user to select artwork
+  }
+}
+
+module.exports = {
+  welcomeMessage,
+  start,
+  displayList,
+  displayFile
+}
